0b2a692f03a89e3f8b3200f81b674cf78480f191
i-supermario <sarang1699@gmail.com>
2022-05-19 21:52:33 +0530

    Added basic code

---
+++ script.js
@@ -0,0 +1,12 @@
+function createGrid(rows,cols){
+
+}
+
+function selectGridSize(evt){
+    let gridRows = prompt("No of grid rows?");
+    let gridCols = prompt("No of grid columns?");
+    function createGrid(gridRows,gridCols);
+}
+
+gridBtn = document.querySelector('.select');
+gridBtn.addEventListener('click',(evt) => selectGridSize(evt));
